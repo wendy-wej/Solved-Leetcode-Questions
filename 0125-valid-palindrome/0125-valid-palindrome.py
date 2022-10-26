47e544231b0d7d4090d445823aa877bf791c4957
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-10-26 12:36:41 +0100

    Time: 57 ms (85.72%), Space: 15.3 MB (21.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alnum_str = ''.join(filter(str.isalnum, s))
-        my_str = alnum_str.lower()
-        print(my_str)
-
-        my_array = list(my_str)
-        print(my_array)
-
-        if not my_array:
+        
+        alnum_str = ''.join(filter(str.isalnum, s)) #removes all non-alphanum and spaces
+        my_str = alnum_str.lower() #converts to lowercase
+        
+        my_array = list(my_str) #make an array
+      
+        #all empty arrays should return true
+        if not my_array: 
             return True
 
+        #my two pointers
         l = 0
         r = len(my_str)-1
 
@@ -21,5 +22,5 @@ class Solution:
                 r -=1
             else:
                 palind = False
-                break
+                break  #no need to continue running
         return palind
