4f4a8e97e6ee727ba79271b5718d767c7b233e4d
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-10-26 12:53:56 +0100

    Time: 259 ms (87.77%), Space: 15.5 MB (73.32%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,14 +3,14 @@ class Solution:
         low_index = 0
         high_index = len(nums) - 1
         
+        if target not in nums:
+                return -1
+            
         while low_index <= high_index:
             mid_index = low_index + ((high_index - low_index)//2)
             midpoint = nums[mid_index]
             
-            if target not in nums:
-                return -1
-            
-            elif target == midpoint:
+            if target == midpoint:
                 return mid_index
             
             elif target >= midpoint:
@@ -20,4 +20,5 @@ class Solution:
                 high_index = mid_index - 1
                     
         return 
+    
         
