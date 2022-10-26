3b050c2a46e8ab95025d63c2315f2bbc968e07dc
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-10-26 20:49:17 +0100

    Time: 60 ms (58.80%), Space: 13.9 MB (64.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        '''
         red = list("0" * (nums.count(0)))
         white = list("1" * (nums.count(1)))
         blue = list("2" * (nums.count(2)))
@@ -12,3 +13,18 @@ class Solution:
         for i in range(len(total)):
             nums[i] = total[i]
         return nums
+        '''
+        red = nums.count(0)
+        white = nums.count(1)
+        blue = nums.count(2)
+        total = red + white + blue
+        for i in range(total):
+            if red >0:
+                nums[i] = 0
+                red -=1
+            elif white > 0:
+                nums[i] = 1
+                white -=1
+            elif blue > 0:
+                nums[i] = 2
+                blue -=1
