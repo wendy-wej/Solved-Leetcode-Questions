e98084f96eada01be033e4c98e0ca6dace9a8932
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-10-30 09:51:45 +0100

    Time: 124 ms (25.41%), Space: 15.1 MB (11.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,3 +6,17 @@ class Solution:
             return False
     
     
+   ##My Slower Solution 
+'''
+def isAnagram(s,t):
+    for i in list(s):
+        if i in t:
+            t = t.replace(i, "", 1)
+        else:
+            return False
+    if len(t)!=0:
+        return False
+    
+    return True
+print(isAnagram(s,t))
+'''
