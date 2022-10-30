d7bd82a7758092018c79b74cf65a55bb05f153ca
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-10-30 03:50:08 +0100

    Time: 130 ms (20.00%), Space: 14.6 MB (26.53%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        for i in list(s):
+            if i in t:
+                t = t.replace(i, "", 1)
+            else:
+                return False
+        if len(t)!=0:
+            return False
+
+        return True
