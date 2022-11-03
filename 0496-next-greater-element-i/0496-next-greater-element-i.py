b968a206adb2ae1f687b27cf042471d0dceec0cd
Chinwendu Nweje <82163426+Wendy2305@users.noreply.github.com>
2022-11-03 10:19:10 +0100

    Time: 163 ms (24.05%), Space: 14.2 MB (56.60%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -5,16 +5,13 @@ class Solution:
         while len(nums_stack1) > 0:
             x = nums_stack1.popleft()
             i = nums2.index(x)
-            if i == len(nums2)-1:
-                stack.append(-1)
-            else:
-                for j in nums2[i+1:]:
-                    if j > x :
-                        stack.append(j)
-                        break
-                    elif j == nums2[-1]:
-                        stack.append(-1)
-                    else:
-                        continue
+            for j in nums2[i:]:
+                if j > x :
+                    stack.append(j)
+                    break
+                elif j == nums2[-1]:
+                    stack.append(-1)
+                else:
+                    continue
 
         return stack
