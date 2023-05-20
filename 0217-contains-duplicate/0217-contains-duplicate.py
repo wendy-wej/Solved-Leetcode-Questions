d3ee290ee80591771025ae1237bd21467b69c67d
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-05-20 18:42:23 +0100

    Time: 533 ms (95.28%), Space: 32 MB (11.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,3 +5,15 @@ class Solution:
         else:
             return True
         
+'''
+Alternative Solution
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
+        This has a runtime of 131ms and a space complexity of 20mb
+'''
