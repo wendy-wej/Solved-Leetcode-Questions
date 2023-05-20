d75403c49692a11b88dec215a81ff879caf1ac70
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-05-20 18:34:10 +0100

    Time: 551 ms (66.32%), Space: 30.8 MB (39.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        set_nums = set(nums)
-        if len(set_nums) == len(nums):
+        if len(set(nums)) == len(nums):
             return False
         else:
             return True
