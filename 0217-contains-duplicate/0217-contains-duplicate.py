6838982f682dc84bbecc29e1da141c9445041b38
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-05-20 18:43:10 +0100

    Time: 554 ms (59.32%), Space: 31 MB (38.10%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(set(nums)) == len(nums):
-            return False
-        else:
-            return True
-        
-'''
-Alternative Solution
         hashset = set()
 
         for n in nums:
@@ -15,5 +8,12 @@ Alternative Solution
             hashset.add(n)
         return False
         
-        This has a runtime of 131ms and a space complexity of 20mb
+        
+'''
+Alternative Solution
+        if len(set(nums)) == len(nums):
+            return False
+        else:
+            return True
+        
 '''
