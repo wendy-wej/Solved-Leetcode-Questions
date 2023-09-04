d589fcd102010d92afc134efd8752fc8ccd5da9e
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-04 01:05:32 +0100

    Time: 65 ms (67.12%), Space: 30.1 MB (39.46%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,32 +1,32 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        word_bank ={}
-        word_set = set()
-        for i in range(len(s)-9):
-            word = s[i:i+10]
-            word_bank[word] = 1+ word_bank.get(word, 0)
-            if word_bank[word] >1:
-                word_set.add(word)
-        return word_set
+        # word_bank ={}
+        # word_set = set()
+        # for i in range(len(s)-9):
+        #     word = s[i:i+10]
+        #     word_bank[word] = 1+ word_bank.get(word, 0)
+        #     if word_bank[word] >1:
+        #         word_set.add(word)
+        # return word_set
             
         
         
         
         
-#         word = ''
-#         start = 0
-#         end = 10
-#         word_bank = {}
-#         ans =set()
+        word = ''
+        start = 0
+        end = 10
+        word_bank = {}
+        ans =set()
         
-#         while (end <= len(s)):
-#             word = s[start:end]
-#             word_bank[word] = 1 + word_bank.get(word, 0)
-#             if word_bank[word] > 1:
-#                 ans.add(word)
-#             start+=1
-#             end+=1
-#         return ans
+        while (end <= len(s)):
+            word = s[start:end]
+            word_bank[word] = 1 + word_bank.get(word, 0)
+            if word_bank[word] > 1:
+                ans.add(word)
+            start+=1
+            end+=1
+        return ans
 
       
             
