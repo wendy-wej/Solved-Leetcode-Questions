91aa711cb85857a13958085d807534529a42c146
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-14 22:51:53 +0100

    Time: 516 ms (21.76%), Space: 39.5 MB (14.10%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -3,5 +3,22 @@ class Solution:
         count = Counter(arr)
         my_list = sorted(arr, key = lambda x:(count[x], x))
         return len(set(my_list[k:]))
+    
+    
+    # <--------------- ALTERNATIVE CODE ----------------------->
+    #Time Comp :faster than 50.03%
+    #space Comp : less than 99.42%
+    
+    # ctr = Counter(arr)
+    #     keys = sorted(ctr.keys(), key = lambda x: ctr[x])
+    #     for key in keys:
+    #         diff = min(k, ctr[key])
+    #         ctr[key] -= diff
+    #         k -= diff
+    #     res = 0
+    #     for key in ctr:
+    #         if ctr[key] > 0:
+    #             res += 1
+    #     return res
             
             
