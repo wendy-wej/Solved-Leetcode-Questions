3fa538418e06d16eb03ad7201061a4ebd8758c12
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-15 13:16:17 +0100

    Time: 1241 ms (72.70%), Space: 55.1 MB (92.09%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,7 +1,6 @@
 class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:                
         intervals.sort()
-        
         remove = 0
         prev_end = intervals[0][1]
         for start, end in intervals[1:]:
@@ -12,6 +11,13 @@ class Solution:
                 prev_end = min(end, prev_end)
         return remove
     
+# STEPS
+# 1. Sort the intervals
+# 2.Iterate through list and remove values who have their ends greater than the prev_end and their start less than the prev_end
+# 3.remove: (end > prev_end) and(start<=prev_end)
+
+    
+    # <--------------------ALTERNATIVE SOLUTION ------------------>
         # intervals = sorted(intervals, key = lambda x:x[1])
         # remove = 0
         # end = -100000
