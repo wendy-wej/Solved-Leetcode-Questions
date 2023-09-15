5ef9bb4bcfdcc2a1499617347f743aaad2432a8c
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-15 17:26:29 +0100

    Time: 141 ms (48.76%), Space: 21.2 MB (34.66%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,22 +1,36 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        
-        prev_start = intervals[0][0]
-        prev_end = intervals[0][1]
         ans= []
-        ans.append([prev_start, prev_end])
+        ans.append(intervals[0])
         
         for start, end in intervals[1:]:
+            prev_start = ans[-1][0]
+            prev_end = ans[-1][1]
             if start > prev_end:
                 ans.append([start, end])
-                prev_end = end
             else:
-                ans.pop()
-                start = min(start, prev_start)
-                end = max(end, prev_end)
-                ans.append([start, end])
-            prev_start = start
-            prev_end = end
-            #print(ans)
+                ans[-1] = [min(start, prev_start),max(end, prev_end)]
         return ans
+
+    #<-----------------ALTERNATIVE SOLUTION ------------>
+    
+#         intervals.sort()     
+#         prev_start = intervals[0][0]
+#         prev_end = intervals[0][1]
+#         ans= []
+#         ans.append([prev_start, prev_end])
+        
+#         for start, end in intervals[1:]:
+#             if start > prev_end:
+#                 ans.append([start, end])
+#                 prev_end = end
+#             else:
+#                 ans.pop()
+#                 start = min(start, prev_start)
+#                 end = max(end, prev_end)
+#                 ans.append([start, end])
+#             prev_start = start
+#             prev_end = end
+#             #print(ans)
+#         return ans
