12e68ee3ab849ae261f2391336c48df3e9db79f4
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-20 19:22:30 +0100

    Time: 41 ms (36.60%), Space: 16.2 MB (94.66%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.py
+++ 0780-reaching-points/0780-reaching-points.py
@@ -1,10 +1,20 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
-        while tx>sx and ty>sy:
-            tx,ty = tx % ty, ty % tx
+#         while tx>sx and ty>sy:
+#             tx,ty = tx % ty, ty % tx
             
-        return (sx==tx and ty>=sy and (ty-sy)%sx == 0)  or \
-                (sy==ty and tx>=sx and (tx-sx)%sy ==0)
+#         return (sx==tx and ty>=sy and (ty-sy)%sx == 0)  or \
+#                 (sy==ty and tx>=sx and (tx-sx)%sy ==0)
+    
+        if sx > tx or sy > ty: return False
+        if sx == tx: return (ty-sy)%sx == 0 # only change y
+        if sy == ty: return (tx-sx)%sy == 0
+        if tx > ty: 
+            return self.reachingPoints(sx, sy, tx%ty, ty) # make sure tx%ty < ty
+        elif tx < ty: 
+            return self.reachingPoints(sx, sy, tx, ty%tx)
+        else:
+            return False
         
         
         # def reachingPoints(self, sx, sy, tx, ty):
