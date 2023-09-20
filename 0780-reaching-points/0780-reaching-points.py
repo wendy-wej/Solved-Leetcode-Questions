3a5a5a90ce8d6f2a752436924756d2233cf7bd16
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-20 19:20:20 +0100

    Time: 50 ms (6.21%), Space: 16.1 MB (99.61%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while tx>sx and ty>sy:
+            tx,ty = tx % ty, ty % tx
+            
+        return (sx==tx and ty>=sy and (ty-sy)%sx == 0)  or \
+                (sy==ty and tx>=sx and (tx-sx)%sy ==0)
+        
+        
+        # def reachingPoints(self, sx, sy, tx, ty):
+        # while sx < tx and sy < ty:
+        #     tx, ty = tx % ty, ty % tx
+        # return sx == tx and sy <= ty and (ty - sy) % sx == 0 or \
+        #        sy == ty and sx <= tx and (tx - sx) % sy == 0
