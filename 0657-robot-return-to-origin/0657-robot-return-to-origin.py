a21031b020ede9872e994ff10a2c3e86572f7d56
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-10-06 23:58:56 +0100

    Time: 65 ms (31.45%), Space: 16.4 MB (38.87%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        l_r = 0
+        u_d = 0
+        for i in moves:
+            if i == 'R':
+                l_r +=1
+            elif i == 'L':
+                l_r -=1
+            elif i == 'U':
+                u_d -=1
+            elif i == 'D':
+                u_d +=1
+            
+        return l_r == 0 and u_d ==0
+        
