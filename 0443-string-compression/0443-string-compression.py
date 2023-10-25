efcb9f10894e94e0794dd5229818a7938559458a
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-10-25 23:16:57 +0100

    Time: 66 ms (51.18%), Space: 16.4 MB (27.37%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,25 +1,42 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        slow= 0
-        fast = 0
-        
-        while fast < len(chars):
-            chars[slow] = chars[fast]
-            count = 1
-            
-            while fast+1 < len(chars) and chars[fast] == chars[fast+1]:
-                count+=1
-                fast+=1
+        count = 1
+        for i in range(len(chars)-1,-1,-1):
+            if i and chars[i]==chars[i-1]:
+                count += 1
+                chars.pop(i)
+            else:
+                if count>1:
+                    for item in str(count)[::-1]:
+                        chars.insert(i+1, item)
+                    count = 1
                 
-            if count>1:    
-                for i in str(count):
-                    chars[slow+1]= i
-                    slow+=1
-                    
-            slow+=1
-            fast+=1
+       
+        
+
+        
+        
+        
+#         slow= 0
+#         fast = 0
+        
+#         while fast < len(chars):
+#             chars[slow] = chars[fast]
+#             count = 1
             
-        return slow
+#             while fast+1 < len(chars) and chars[fast] == chars[fast+1]:
+#                 count+=1
+#                 fast+=1
+                
+#             if count>1:    
+#                 for i in str(count):
+#                     chars[slow+1]= i
+#                     slow+=1
+                    
+#             slow+=1
+#             fast+=1
+            
+#         return slow
                 
         
                 
