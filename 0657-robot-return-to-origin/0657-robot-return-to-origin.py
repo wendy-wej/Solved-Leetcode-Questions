910690cff756b3cc0092792779d07a601be2d41e
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-10-26 00:21:35 +0100

    Time: 45 ms (85.27%), Space: 16.3 MB (64.96%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,9 +1,7 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        if (moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")):
-            return True
-        else:
-            return False
+        return (moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R"))
+            
         
         
         
