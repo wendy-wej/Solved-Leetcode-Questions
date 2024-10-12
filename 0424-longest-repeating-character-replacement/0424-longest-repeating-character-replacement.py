b911866659d77a28eca58e6a86b92749d703ae28
Chinwendu Nweje <wendu.nweje@gmail.com>
2024-10-12 23:59:51 +0100

    Time: 88 ms (62.07%), Space: 16.8 MB (10.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,13 +3,15 @@ class Solution:
         word_dict = dict()
         ans = 0
         l = r = 0
+        maxf = 0
         for r in range(len(s)):
             if s[r] in word_dict:
                 word_dict[s[r]] +=1
             else: 
                 word_dict[s[r]] = 1
                 
-            while (r-l+1) - max(word_dict.values()) > k:
+            maxf = max(maxf, word_dict[s[r]] )
+            while (r-l+1) - maxf > k:
                 word_dict[s[l]] -=1
                 l+=1
             ans = max(ans, r-l+1)    
