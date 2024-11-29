e86db0170aec035f91a440ed7bf20f37492604a4
Chinwendu Nweje <wendu.nweje@gmail.com>
2024-11-29 10:41:56 +0100

    Time: 228 ms (82.49%), Space: 19.8 MB (41.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,30 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: List[List[str]]) -> int:           
         if not grid:
             return 0
         
         R = len(grid)
         C = len(grid[0])
-        path = set()
         count = 0
         
-        def dfs(r,c):
-            if (r<0 or c<0 or 
-                r>=R or c>=C or 
-                grid[r][c]=='0' or (r,c) in path):
-                return 
-            grid[r][c] = '0'
-            path.add((r,c))
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-            path.remove((r,c))
-    
         for r in range(R):
             for c in range(C):
                 if grid[r][c] == '1':
-                    dfs(r,c)
+                    self.dfs(grid, r,c)
                     count+=1
         return count
+    
+    def dfs(self, grid, r,c):
+        R = len(grid)
+        C = len(grid[0])
+        if (r<0 or c<0 or 
+            r>=R or c>=C or 
+            grid[r][c]=='0'):
+            return 
+        grid[r][c] = '0'
+
+        self.dfs(grid, r+1, c)
+        self.dfs(grid, r-1, c)
+        self.dfs(grid, r, c+1)
+        self.dfs(grid, r, c-1)
         
